b44f1663dc3234eb2f99e333b9acdc6535504dd9
Mike Rachman <mikerachman1@gmail.com>
2023-01-26 12:20:29 -0500

    made header sticky

--- src/Header.js
+++ src/Header.js
@@ -8,13 +8,21 @@ const Header = () => {
       </div>
       <div className="characters">
         <img src="https://where-s-waldo-eece1.web.app/static/media/waldo.92dd6e4f.webp" 
-              alt="waldo" />
+              alt="waldo" 
+              id="waldo-sticky"
+              />
         <img src="https://where-s-waldo-eece1.web.app/static/media/wenda.c33345bb.webp" 
-              alt="wenda" />
+              alt="wenda" 
+              id="wenda-sticky"
+              />
         <img src="https://where-s-waldo-eece1.web.app/static/media/wizard.f54f77e5.webp" 
-              alt="wizard" />
+              alt="wizard"
+              id="wenda-sticky"
+              />
         <img src="https://where-s-waldo-eece1.web.app/static/media/odlaw.46f85d87.webp" 
-              alt="odlaw" />      
+              alt="odlaw" 
+              id="odlaw-sticky"
+              />      
       </div>
     </div>
   );
