0c13bd46ced7117af65b1211d45153ee3f2af073
Mike Rachman <mikerachman1@gmail.com>
2023-01-27 17:36:29 -0500

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeZkVLydkeH6IuFi1LzUj1F-R6udFIrSM",
+  authDomain: "waldo-66e46.firebaseapp.com",
+  projectId: "waldo-66e46",
+  storageBucket: "waldo-66e46.appspot.com",
+  messagingSenderId: "1092461110521",
+  appId: "1:1092461110521:web:6ab8893332250b6eaa2e28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
